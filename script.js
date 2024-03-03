0b1a8c1443ebeee836a6403c6138f53d7f739228
DSS-SFSU <162064201+DSS-SFSU@users.noreply.github.com>
2024-03-03 15:27:14 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+<button onclick="changeColor()">Change Color</button>
+<p id="text">Hello, world!</p>
+
+<script>
+function changeColor() {
+    document.getElementById("text").style.color = "red";
+}
+</script>
